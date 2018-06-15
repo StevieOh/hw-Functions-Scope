2ac79d07798ca0db0a68fd8c5c1e8f9d8e2cb2f2
Stephanie Obeytek <sobeytek@gmail.com>
2018-06-14 19:24:04 -0500

    Commit 2 - Palindrome

--- js/app.js
+++ js/app.js
@@ -1,1 +1,34 @@
 console.log("JavaScript is working!");
+
+// 1. There is no "question 1"
+// 
+// 2. What is the difference between a parameter and an argument?
+    // A parameter is what your code is constricted by
+    // An argument is something that defines your code
+    // 
+// 3. Within a function, what is the difference between return and console.log?
+   // "Return" will end the loop or function, so the code below will not be executed.
+   // Console.log will display the parameter in the  browser console.
+   // 
+
+  // Write a function checkPalindrome that accepts a single argument, a string. The function should return true (Boolean) if the string is a palindrome, false if it is not. Make sure your function will give the correct answer for words with capital letters.
+  const checkPalindrome = (pdm) => {
+    // console.log(pdm);
+    const lower = pdm.toLowerCase()
+    console.log(lower);
+    const array = lower.split("");
+    // console.log(array);
+    const rev = array.reverse();
+    const back = (rev.join(""));
+    console.log(back);
+    if (back === lower){
+      console.log(true);
+    }else {
+      console.log( false);
+
+    }
+  }
+  checkPalindrome("Radar");
+//  //=> true
+  checkPalindrome("Borscht");
+// //=> false
